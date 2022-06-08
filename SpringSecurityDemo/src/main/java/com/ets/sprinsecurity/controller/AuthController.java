4645590de94e23d9916467a1a3f10beac8d26740
Syawadhilah Pradipta <dipta0988@gmail.com>
2022-06-08 10:18:48 -0700

    update

---
+++ SpringSecurityDemo/src/main/java/com/ets/sprinsecurity/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.ets.sprinsecurity.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ets.sprinsecurity.payload.request.SignupRequest;
+import com.ets.sprinsecurity.response.MessageResponse;
+import com.ets.sprinsecurity.service.repo.RoleRepository;
+import com.ets.sprinsecurity.service.repo.UserRepository;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	RoleRepository roleRepository;
+	
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid SignupRequest signupRequest){
+		
+		//username should not be existing one
+		if(userRepository.existsByUsername(signupRequest.getUsername())){
+			return ResponseEntity.badRequest().body(new MessageResponse("error : username is already taken"));
+		}
+		
+		if(userRepository.existsByEmail(signupRequest.getEmail())) {
+			return ResponseEntity.badRequest().body(new MessageResponse("error: email is already taken"));
+		}
+	}
+}
